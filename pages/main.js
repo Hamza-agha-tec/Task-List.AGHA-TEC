82dd714c387e9663197ab05b78d643b646c15269
Hamza-agha-tec <129862726+Hamza-agha-tec@users.noreply.github.com>
2023-07-15 08:05:24 +0100

    Update main.js

--- pages/main.js
+++ pages/main.js
@@ -1,7 +1,1 @@
-	document.getElementById("copy-button").addEventListener("click", function() {
-	  var copyText = document.getElementById("text-to-copy");
-	  copyText.select();
-	  copyText.setSelectionRange(0, 99999); // For mobile devices
-	  document.execCommand("copy");
-	  alert("Acount Number Copied : " + copyText.value);
-	});
+
